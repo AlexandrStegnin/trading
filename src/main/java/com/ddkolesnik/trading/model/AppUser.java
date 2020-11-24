2967cebc1e05124c7dec256a33b5bb2ef72cc1b5
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-24 14:42:31 +0500

    Добавлен класс пользователь в систему.

--- src/main/java/com/ddkolesnik/trading/model/AppUser.java
+++ src/main/java/com/ddkolesnik/trading/model/AppUser.java
@@ -1,8 +1,41 @@
 package com.ddkolesnik.trading.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * @author Alexandr Stegnin
  */
 
-public class AppUser {
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "app_user")
+@EqualsAndHashCode(callSuper = true)
+public class AppUser extends BaseEntity {
+
+    @Size(max = 45, message = "Логин не должен содержать более {max} символов")
+    @Column(name = "login", unique = true, updatable = false, nullable = false)
+    private String login;
+
+    @NotBlank(message = "Пароль должен быть задан")
+    @Column(name = "password")
+    private String password;
+
+    @Email(message = "Нужно указать валидный email [example@example.ru]")
+    @Column(name = "email")
+    private String email;
+
+    public AppUser(UserDetails principal) {
+        this.login = principal.getUsername();
+    }
 }
