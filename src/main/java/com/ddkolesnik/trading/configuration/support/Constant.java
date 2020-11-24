a2c30bbad0dbfde078594e9758d5c7ee2ed7fc46
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-24 15:21:52 +0500

    Добавлен вспомогательный класс для хранения констант.

---
+++ src/main/java/com/ddkolesnik/trading/configuration/support/Constant.java
@@ -0,0 +1,16 @@
+package com.ddkolesnik.trading.configuration.support;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public class Constant {
+
+    //    App roles constants
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+    public static final String ROLE_ADMIN = ROLE_PREFIX + ADMIN;
+    public static final String ROLE_USER = USER;
+
+}
