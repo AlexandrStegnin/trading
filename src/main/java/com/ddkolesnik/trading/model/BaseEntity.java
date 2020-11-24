a171845b91fdfd72fb048633322d3745723795bb
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-24 14:36:12 +0500

    Добавлена базовая сущность.

---
+++ src/main/java/com/ddkolesnik/trading/model/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.ddkolesnik.trading.model;
+
+import com.ddkolesnik.trading.configuration.SecurityUtils;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    private Long id;
+
+    @Column(name = "creation_time")
+    private Instant creationTime;
+
+    @Column(name = "modified_time")
+    private Instant modifiedTime;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @PrePersist
+    public void prePersist() {
+        this.creationTime = Instant.now();
+        this.modifiedBy = SecurityUtils.getUsername();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedTime = Instant.now();
+        this.modifiedBy = SecurityUtils.getUsername();
+    }
+
+}
