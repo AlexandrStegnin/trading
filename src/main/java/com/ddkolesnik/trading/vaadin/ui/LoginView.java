95904585f6626900a2a60a7d42b8026cc2e23a3d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-24 16:07:09 +0500

    Добавлена страница входа в систему.

---
+++ src/main/java/com/ddkolesnik/trading/vaadin/ui/LoginView.java
@@ -0,0 +1,60 @@
+package com.ddkolesnik.trading.vaadin.ui;
+
+import com.ddkolesnik.trading.configuration.support.Internationalization;
+import com.ddkolesnik.trading.repository.AuthRepository;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.login.AbstractLogin;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import static com.ddkolesnik.trading.configuration.support.Location.LOGIN_PAGE;
+import static com.ddkolesnik.trading.configuration.support.Location.TRADING_PAGE;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@PageTitle(LoginView.PAGE_TITLE)
+@Route(value = LOGIN_PAGE)
+public class LoginView extends HorizontalLayout {
+
+    protected static final String PAGE_TITLE = "АВТОРИЗАЦИЯ";
+
+    private final AuthRepository authRepository;
+
+    public LoginView(AuthRepository authRepository) {
+        this.authRepository = authRepository;
+        init();
+    }
+
+    private void init() {
+        LoginOverlay loginForm = new LoginOverlay();
+//        Div title = new Div();
+//        title.add(VaadinViewUtils.getLogo(350));
+//        loginForm.setTitle(title);
+        loginForm.setDescription("");
+        loginForm.setOpened(true);
+        loginForm.setI18n(Internationalization.russianI18n());
+        loginForm.setForgotPasswordButtonVisible(false);
+        loginForm.addLoginListener(e -> {
+            boolean authenticated = authenticate(e);
+            if (authenticated) {
+                loginForm.close();
+                UI.getCurrent().getPage().setLocation(TRADING_PAGE);
+            } else {
+                loginForm.setError(true);
+            }
+        });
+        setAlignItems(Alignment.CENTER);
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        add(loginForm);
+    }
+
+    private boolean authenticate(AbstractLogin.LoginEvent e) {
+        return authRepository.authenticate(e.getUsername(), e.getPassword()).isAuthenticated();
+    }
+
+}
