a4bc203cc68bf135266449b8660ff64b7fc42b5e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-25 10:08:51 +0500

    Доработана форма отображения данных о торгах. Добавлена колонка с checkbox для выбора нескольких строк.

--- src/main/java/com/ddkolesnik/trading/vaadin/ui/TradingView.java
+++ src/main/java/com/ddkolesnik/trading/vaadin/ui/TradingView.java
@@ -1,15 +1,13 @@
 package com.ddkolesnik.trading.vaadin.ui;
 
-import com.ddkolesnik.trading.configuration.support.OperationEnum;
 import com.ddkolesnik.trading.model.TradingEntity;
 import com.ddkolesnik.trading.service.AppUserService;
 import com.ddkolesnik.trading.service.TradingService;
 import com.ddkolesnik.trading.vaadin.custom.CustomAppLayout;
-import com.ddkolesnik.trading.vaadin.form.TradingForm;
-import com.ddkolesnik.trading.vaadin.support.VaadinViewUtils;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
@@ -18,7 +16,9 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.material.Material;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import static com.ddkolesnik.trading.configuration.support.Location.TRADING_PAGE;
 
@@ -36,26 +36,24 @@ public class TradingView extends CustomAppLayout {
     private final TradingService tradingService;
     private final Grid<TradingEntity> grid;
     private final ListDataProvider<TradingEntity> dataProvider;
-    private TradingForm tradingForm;
+    private final CheckboxGroup<Checkbox> checkboxGroup;
+    private final Set<Checkbox> items;
 
     public TradingView(TradingService tradingService, AppUserService userService) {
         super(userService);
         this.tradingService = tradingService;
         this.grid = new Grid<>();
         this.dataProvider = new ListDataProvider<>(getAll());
+        this.checkboxGroup = new CheckboxGroup<>();
+        this.items = new LinkedHashSet<>();
         init();
     }
 
     private void init() {
         grid.setDataProvider(dataProvider);
 
-        grid.addColumn(TradingEntity::getLot)
-                .setHeader("ЛОТ")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getDescription)
-                .setHeader("ОПИСАНИЕ")
+        grid.addColumn(TradingEntity::getCity)
+                .setHeader("ГОРОД")
                 .setTextAlign(ColumnTextAlign.CENTER)
                 .setFlexGrow(1);
 
@@ -64,63 +62,39 @@ public class TradingView extends CustomAppLayout {
                 .setTextAlign(ColumnTextAlign.CENTER)
                 .setFlexGrow(1);
 
-        grid.addColumn(TradingEntity::getTradingNumber)
-                .setHeader("НОМЕР ТОРГОВ")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getEfrsbId)
-                .setHeader("ИДЕНТИФИКАТОР В ЕФРСБ")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getAuctionStep)
-                .setHeader("ШАГ АУКЦИОНА")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getDepositAmount)
-                .setHeader("СУММА ЗАДАТКА")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getTradingTime)
-                .setHeader("ВРЕМЯ ТОРГОВ")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getAcceptRequestsDate)
-                .setHeader("ПРИЁМ ЗАЯВОК")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
-
-        grid.addColumn(TradingEntity::getUrl)
-                .setHeader("ССЫЛКА")
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setFlexGrow(1);
+        //todo квадратура
 
         grid.addColumn(TradingEntity::getPrice)
                 .setHeader("СТОИМОСТЬ")
                 .setTextAlign(ColumnTextAlign.CENTER)
                 .setFlexGrow(1);
 
-        grid.addColumn(TradingEntity::getSeller)
-                .setHeader("ПРОДАВЕЦ")
+        //todo источник
+
+        grid.addColumn(TradingEntity::getUrl)
+                .setHeader("ССЫЛКА")
                 .setTextAlign(ColumnTextAlign.CENTER)
                 .setFlexGrow(1);
 
-        grid.addColumn(TradingEntity::getCity)
-                .setHeader("ГОРОД")
+        Checkbox selectAll = createSelectAll();
+
+        grid.addComponentColumn(this::createCheckBox)
+                .setHeader(selectAll)
                 .setTextAlign(ColumnTextAlign.CENTER)
                 .setFlexGrow(1);
 
-        grid.addComponentColumn(role -> VaadinViewUtils.makeEditorColumnActions(
-                e -> showAppTokenForm(OperationEnum.UPDATE, role),
-                e -> showAppTokenForm(OperationEnum.DELETE, role)))
-                .setTextAlign(ColumnTextAlign.CENTER)
-                .setEditorComponent(new Div())
-                .setFlexGrow(2)
-                .setHeader("ДЕЙСТВИЯ");
+        checkboxGroup.addValueChangeListener(event -> {
+            if (event.getValue().size() == items.size()) {
+                selectAll.setValue(true);
+                selectAll.setIndeterminate(false);
+            } else if (event.getValue().size() == 0) {
+                selectAll.setValue(false);
+                selectAll.setIndeterminate(false);
+            } else {
+                selectAll.setIndeterminate(true);
+            }
+        });
+
         VerticalLayout verticalLayout = new VerticalLayout();
         verticalLayout.add(grid);
         verticalLayout.setAlignItems(FlexComponent.Alignment.END);
@@ -131,19 +105,35 @@ public class TradingView extends CustomAppLayout {
         return tradingService.findAll();
     }
 
-    private void showAppTokenForm(final OperationEnum operation, final TradingEntity entity) {
-        TradingForm tradingForm = new TradingForm(operation, entity, tradingService);
-        this.tradingForm = tradingForm;
-        tradingForm.addOpenedChangeListener(e -> refreshDataProvider(e.isOpened(), operation, entity));
-        tradingForm.open();
+    private void confirm(final TradingEntity entity) {
+        tradingService.confirm(entity);
     }
 
-    private void refreshDataProvider(final boolean isOpened, final OperationEnum operation, final TradingEntity entity) {
-        if (!isOpened && !tradingForm.isCanceled()) {
-            if (operation.compareTo(OperationEnum.CREATE) == 0) dataProvider.getItems().add(entity);
-            else if (operation.compareTo(OperationEnum.DELETE) == 0) dataProvider.getItems().remove(entity);
-            dataProvider.refreshAll();
-        }
+    private Checkbox createCheckBox(TradingEntity entity) {
+        Checkbox checkbox = new Checkbox();
+        checkbox.setValue(entity.isConfirmed());
+        checkbox.addValueChangeListener(event -> {
+            if (event.getValue()) {
+                checkboxGroup.select(checkbox);
+            } else {
+                checkboxGroup.deselect(checkbox);
+            }
+        });
+        items.add(checkbox);
+        checkboxGroup.setItems(items);
+        return checkbox;
+    }
+
+    private Checkbox createSelectAll() {
+        Checkbox selectAll = new Checkbox();
+        selectAll.addValueChangeListener(event -> {
+            if (selectAll.getValue()) {
+                items.forEach(checkbox -> checkbox.setValue(true));
+            } else {
+                items.forEach(checkbox -> checkbox.setValue(false));
+            }
+        });
+        return selectAll;
     }
 
 }
