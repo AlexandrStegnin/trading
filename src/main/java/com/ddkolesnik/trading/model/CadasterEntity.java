0d0a8a7e8137dd713e56a9a9f8a20ee294661e7e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-12-05 22:06:43 +0500

    Добавлен конструктор без параметров и конструктор на основе DTO.

--- src/main/java/com/ddkolesnik/trading/model/CadasterEntity.java
+++ src/main/java/com/ddkolesnik/trading/model/CadasterEntity.java
@@ -1,7 +1,9 @@
 package com.ddkolesnik.trading.model;
 
+import com.ddkolesnik.trading.model.dto.CadasterDTO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,6 +15,7 @@ import javax.persistence.Table;
 
 @Data
 @Entity
+@NoArgsConstructor
 @Table(name = "cadaster")
 @EqualsAndHashCode(callSuper = true)
 public class CadasterEntity extends BaseEntity {
@@ -34,5 +37,14 @@ public class CadasterEntity extends BaseEntity {
 
     @Column(name = "tag")
     private String tag;
+
+    public CadasterEntity(CadasterDTO dto, String tag) {
+        this.address = dto.getAddress();
+        this.cadNumber = dto.getCadNumber();
+        this.type = dto.getType();
+        this.area = dto.getArea();
+        this.category = dto.getCategory();
+        this.tag = tag;
+    }
 
 }
