6447c15943a382b82122743009f95f111bf5a05b
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-12-07 13:52:26 +0500

    Добавлены конструкторы в класс.

--- src/main/java/com/ddkolesnik/trading/model/RosreestrRequest.java
+++ src/main/java/com/ddkolesnik/trading/model/RosreestrRequest.java
@@ -1,18 +1,29 @@
 package com.ddkolesnik.trading.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Alexandr Stegnin
  */
 
 @Data
+@NoArgsConstructor
 public class RosreestrRequest {
 
-    private String mode = "normal";
+    private String mode;
 
     private String query;
 
-    private int grouped = 1;
+    private int grouped;
+
+    public RosreestrRequest(String mode, String query, int grouped) {
+        this.mode = mode;
+        this.grouped = grouped;
+    }
+
+    public RosreestrRequest(String query) {
+        this.query = query;
+    }
 
 }
