667bbd992ffe3a7c2d6d00ceb32e8dbafbcc183d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-12-28 18:09:41 +0500

    Добавлен web клиент для работы с ФИАС

--- src/main/java/com/ddkolesnik/trading/configuration/AppConfig.java
+++ src/main/java/com/ddkolesnik/trading/configuration/AppConfig.java
@@ -20,12 +20,27 @@ public class AppConfig {
     @Value("${app.api.base.url}")
     private String appApiBaseUrl;
 
+    @Value("${app.api.fias.base.url}")
+    private String fiasApiBaseUrl;
+
+    @Value("${app.api.fias.token}")
+    private String appFiasApiToken;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl(appApiBaseUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader("Token", appApiToken)
+                .build();
+    }
+
+    @Bean
+    public WebClient fiasWebClient() {
+        return WebClient.builder()
+                .baseUrl(fiasApiBaseUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("Token", appFiasApiToken)
                 .build();
     }
 
