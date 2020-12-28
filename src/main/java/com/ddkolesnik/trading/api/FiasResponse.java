84aee3f0f559b413506c2573916d0320abc5b7b0
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-12-28 20:01:42 +0500

    Изменён метод получения результата от сервиса ФИАС.

--- src/main/java/com/ddkolesnik/trading/api/FiasResponse.java
+++ src/main/java/com/ddkolesnik/trading/api/FiasResponse.java
@@ -1,6 +1,11 @@
 package com.ddkolesnik.trading.api;
 
+import com.ddkolesnik.trading.model.dto.FiasResponseDTO;
+import com.ddkolesnik.trading.model.dto.FiasSearchContextDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * @author Alexandr Stegnin
@@ -8,4 +13,11 @@ import lombok.Data;
 
 @Data
 public class FiasResponse {
+
+    @JsonProperty("searchContext")
+    private FiasSearchContextDTO searchContext;
+
+    @JsonProperty("result")
+    private List<FiasResponseDTO> result;
+
 }
