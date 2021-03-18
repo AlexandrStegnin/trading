372f8d5fe710b53bdbfaab77bd9440c6d7afc094
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-03-18 17:01:56 +0500

    Добавлено DTO для взаимодействия с Trello

---
+++ src/main/java/com/ddkolesnik/trading/model/dto/TrelloDTO.java
@@ -0,0 +1,22 @@
+package com.ddkolesnik.trading.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TrelloDTO {
+
+    @JsonProperty("name")
+    String name;
+
+    @JsonProperty("idMembers")
+    String[] idMembers;
+
+}
