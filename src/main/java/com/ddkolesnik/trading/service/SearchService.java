d838e6b6f255a27b506b3f7a99cf703de23f1f22
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-04-19 13:15:21 +0500

    Рефакторинг. Удалены неиспользуемые методы.

--- src/main/java/com/ddkolesnik/trading/service/SearchService.java
+++ src/main/java/com/ddkolesnik/trading/service/SearchService.java
@@ -1,11 +1,9 @@
 package com.ddkolesnik.trading.service;
 
-import com.ddkolesnik.trading.api.*;
+import com.ddkolesnik.trading.api.CadasterEntity;
+import com.ddkolesnik.trading.api.KadnetData;
+import com.ddkolesnik.trading.api.KadnetResponse;
 import com.ddkolesnik.trading.configuration.security.SecurityUtils;
-import com.ddkolesnik.trading.model.dto.CadasterDTO;
-import com.ddkolesnik.trading.model.dto.EgrnDTO;
-import com.ddkolesnik.trading.model.dto.EgrnDetailsDTO;
-import com.ddkolesnik.trading.model.dto.FiasResponseDTO;
 import com.ddkolesnik.trading.service.client.ApiClient;
 import com.ddkolesnik.trading.vaadin.support.VaadinViewUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -57,22 +55,6 @@ public class SearchService {
      *
      * @param address адрес для поиска в ФИАС
      */
-    public void getFiasResponse(String address) {
-        Mono<FiasResponse> response = client.getFiasResponse(address);
-        log.info("Получаем данные из ФИАС");
-        String userName = SecurityUtils.getUsername();
-        FiasResponse fiasResponse = response.block();
-        if (fiasResponse != null) {
-            getRosreestrInfo(fiasResponse, userName, address);
-            log.info("Закончили");
-        }
-    }
-
-    /**
-     * Получить список доступных похожих адресов
-     *
-     * @param address адрес для поиска в ФИАС
-     */
     public void getKadnetResponse(String address) {
         Mono<KadnetResponse> response = client.getKadnetResponse(address);
         log.info("Получаем данные из КАДНЕТ");
@@ -82,20 +64,6 @@ public class SearchService {
             getRosreestrInfo(kadnetResponse, userName, address);
             log.info("Закончили");
         }
-    }
-
-    private void getRosreestrInfo(FiasResponse response, String userName, String tag) {
-        if (response.getResult() == null) {
-            VaadinViewUtils.showNotification("Не удалось получить ответ от ФИАС");
-            return;
-        }
-        List<String> addresses = response.getResult()
-                .stream()
-                .filter(fiasResponseDTO -> fiasResponseDTO.getContentType().equalsIgnoreCase("building"))
-                .map(FiasResponseDTO::getFullName)
-                .collect(Collectors.toList());
-        log.info("Начинаем разбор адресов [{} шт], полученных из ФИАС", addresses.size());
-        addresses.forEach(address -> getRosreestrInfo(address, userName, tag));
     }
 
     private void getRosreestrInfo(KadnetResponse response, String userName, String tag) {
@@ -114,66 +82,6 @@ public class SearchService {
             daDataService.cleanData(entity);
             cadasterService.create(entity);
         });
-    }
-
-    /**
-     * Получить ответ от Росреестра по адресу
-     *  @param address адрес
-     * @param userName имя текущего пользователя
-     * @param tag тэг под которым надо сохранить инфо
-     */
-    private void getRosreestrInfo(String address, String userName, String tag) {
-        RosreestrRequest request = new RosreestrRequest("normal", address, 1);
-        Mono<RosreestrResponse> mono = client.getRosreestrResponse(request);
-        RosreestrResponse rosreestrResponse = mono.block();
-        if (rosreestrResponse != null) {
-            if (rosreestrResponse.getError().toString().contains("SEARCH_OVERLIMIT")) {
-                VaadinViewUtils.showNotification("Превышен лимит запросов в Росреестр (apiegrn.ru)");
-                return;
-            }
-            log.info("Обновляем данные, полученные из Росреестра. Добавляем тип и этаж.");
-            getEgrnDetails(rosreestrResponse, userName, tag);
-        }
-    }
-
-    /**
-     * Получить детализированную информацию по кадастровому номеру
-     *
-     * @param rosreestrResponse ответ Росреестра
-     */
-    private void getEgrnDetails(RosreestrResponse rosreestrResponse, String userName, String tag) {
-        List<CadasterDTO> allObjects = rosreestrResponse.getObject().getAllObjects();
-        log.info("Получаем детали по [{} шт] объектам", allObjects.size());
-        allObjects.forEach(dto -> {
-            CadasterEntity entity = new CadasterEntity(dto, tag, userName);
-            RosreestrRequest request = new RosreestrRequest(entity.getCadNumber());
-            Mono<EgrnResponse> mono = client.getCadasterDetails(request);
-            EgrnResponse egrnResponse = mono.block();
-            updateCadaster(egrnResponse, entity);
-        });
-    }
-
-    /**
-     * Обновить запись в базе данных
-     *
-     * @param response ответ с детальной информацией
-     * @param entity   сущность для обновления
-     */
-    private void updateCadaster(EgrnResponse response, CadasterEntity entity) {
-        if (response != null) {
-            EgrnDTO egrnDTO = response.getEgrnDTO();
-            if (egrnDTO != null) {
-                EgrnDetailsDTO detailsDTO = egrnDTO.getDetails();
-                if (detailsDTO.getOksType() != null) {
-                    entity.setType(detailsDTO.getOksType());
-                }
-                if (detailsDTO.getFloor() != null) {
-                    entity.setFloor(detailsDTO.getFloor());
-                }
-                daDataService.cleanData(entity);
-                cadasterService.create(entity);
-            }
-        }
     }
 
     /**
